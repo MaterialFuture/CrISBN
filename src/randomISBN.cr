4c4fd909a2d851553e3190a2bb5cb789eb60123d
Konstantine <konstantinev@protonmail.com>
2020-02-09 22:17:58 -0600

    Cleanup main file, and cleanup rand

--- src/randomISBN.cr
+++ src/randomISBN.cr
@@ -1,19 +1,5 @@
-require "./randomISBN/english.cr"
+require "./RandomISBN/english.cr"
 
 module RandomISBN
-  VERSION = "0.2.2"
-
-  puts RandomISBN::English.info
-  rand = RandomISBN::English.new
-
-  # Check to make sure that it prints out a 13 digit number
-  while (rand.size < 13)
-    rand = RandomISBN::English.new
-  end
-
-  # The main part of the app, returns the value as a string.
-  puts "New ISBN-13: " + rand
-
-  # Size for testing to make sure the while loop works, Remove once tests are in place
-  puts "Size of string: " + (rand.size).to_s
+  VERSION = "0.2.3"
 end
--- src/randomISBN/english.cr
+++ src/randomISBN/english.cr
@@ -15,24 +15,24 @@ class RandomISBN::English
       digit_codes = Random.new.rand(1..6)
       case digit_codes
       when 6
-        rand_byte = RandNum.new.fromZero 7
+        rand_byte = RandNum.fromZero 7
         reg_group = Random.new.rand(1)
       when 5
-        rand_byte = RandNum.new.fromZero 6
+        rand_byte = RandNum.fromZero 6
         reg_group_rand = [200..227, 229..368, 370..638, 640..647, 649..654, 656..699].sample()
         reg_group = Random.new.rand(reg_group_rand)
       when 4
-        rand_byte = RandNum.new.fromZero 5
+        rand_byte = RandNum.fromZero 5
         reg_group_rand = [2280..2289, 3690..3699, 6390..6398, 6550..6559, 7000..8499].sample()
         reg_group = Random.new.rand(reg_group_rand)
       when 3
-        rand_byte = RandNum.new.fromZero 4
+        rand_byte = RandNum.fromZero 4
         reg_group = Random.new.rand(85000..89999)
       when 2
-        rand_byte = RandNum.new.fromZero 3
+        rand_byte = RandNum.fromZero 3
         reg_group = Random.new.rand(900000..949999)
       when 1
-        rand_byte = RandNum.new.fromZero 2
+        rand_byte = RandNum.fromZero 2
         reg_group_rand = [6399000..6399999, 6480000..6489999, 9500000..9999999].sample()
         reg_group = Random.new.rand(reg_group_rand)
       end
@@ -44,32 +44,32 @@ class RandomISBN::English
       # Check if the digit_codes match and generate the rest of the numbers accordingly
       case digit_codes
       when 6
-        rand_byte = RandNum.new.fromZero 7
+        rand_byte = RandNum.fromZero 7
         reg_group = "0" + Random.new.rand(1..6).to_s
       when 5
-        rand_byte = RandNum.new.fromZero 6
+        rand_byte = RandNum.fromZero 6
         reg_group_rand = [0..9, 100..397, 714..716].sample()
         reg_group = Random.new.rand(reg_group_rand)
         if (reg_group < 10) #Todo: Cleanup by making this a ternary to be cleaner
           reg_group = "00" + reg_group.to_s
         end
       when 4
-        rand_byte = RandNum.new.fromZero 5
+        rand_byte = RandNum.fromZero 5
         reg_group_rand = [700..999, 3980..5499, 6860..7139, 7170..7319, 7900..7999, 8672..8675, 9730..9877].sample()
         reg_group = Random.new.rand(reg_group_rand)
         if (reg_group > 700 && reg_group < 999) #Todo: Cleanup by making this a ternary to be cleaner
           reg_group = "0" + reg_group.to_s
         end
       when 3
-        rand_byte = RandNum.new.fromZero 4
+        rand_byte = RandNum.fromZero 4
         reg_group_rand = [55000..68599, 74000..77499, 77540..77639, 77650..77699, 77770..78999, 80000..86719, 86760..86979].sample()
         reg_group = Random.new.rand(reg_group_rand)
       when 2
-        rand_byte = RandNum.new.fromZero 3
+        rand_byte = RandNum.fromZero 3
         reg_group_rand = [869800..915999, 916506..972999, 987800..998999].sample()
         reg_group = Random.new.rand(reg_group_rand)
       when 1
-        rand_byte = RandNum.new.fromZero 2
+        rand_byte = RandNum.fromZero 2
         reg_group_rand = [7320000..7399999, 7750000..7753999, 7764000..7764999, 7770000..7776999, 9160000..9165059, 9990000..9999999].sample()
         reg_group = Random.new.rand(reg_group_rand)
       end
