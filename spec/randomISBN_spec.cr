24395976c36ab9cfd5d87099eae5e9147c67a508
Konstantine <konstantinev@protonmail.com>
2020-02-10 01:13:07 -0600

    updated tests

--- spec/randomISBN_spec.cr
+++ spec/randomISBN_spec.cr
@@ -4,34 +4,37 @@ describe RandomISBN do
   it "Main Crystal File works" do
     RandomISBN.should_not eq(false || false) #Shouldn't be true or false
   end
-  it "Checks the size of the English ISBN to make sure it's 16" do
+  it "Checks the size of the English ISBN to make sure it's 13" do
     ((RandomISBN::English.new).size).should eq(13) #Make sure that it always prints 13 digits
+  end
+  it "Checks to make sure the radom isbn is valid" do
+    (RandomISBN::IsValid.new(RandomISBN::English.new)).should eq(true) #Make sure that it always prints 13 digits
   end
 end
 
 describe RandNum do
   it "Check RandNum, FromZero Works" do
-    (RandNum.new.fromZero(1)).size.should eq(1)
-    (RandNum.new.fromZero(2)).size.should eq(2)
-    (RandNum.new.fromZero(3)).size.should eq(3)
-    (RandNum.new.fromZero(4)).size.should eq(4)
-    (RandNum.new.fromZero(5)).size.should eq(5)
-    (RandNum.new.fromZero(6)).size.should eq(6)
-    (RandNum.new.fromZero(7)).size.should eq(7)
-    (RandNum.new.fromZero(8)).size.should eq(8)
-    (RandNum.new.fromZero(9)).size.should eq(9)
-    (RandNum.new.fromZero(0)).size.should eq(0)
+    (RandNum.fromZero(1)).size.should eq(1)
+    (RandNum.fromZero(2)).size.should eq(2)
+    (RandNum.fromZero(3)).size.should eq(3)
+    (RandNum.fromZero(4)).size.should eq(4)
+    (RandNum.fromZero(5)).size.should eq(5)
+    (RandNum.fromZero(6)).size.should eq(6)
+    (RandNum.fromZero(7)).size.should eq(7)
+    (RandNum.fromZero(8)).size.should eq(8)
+    (RandNum.fromZero(9)).size.should eq(9)
+    (RandNum.fromZero(0)).size.should eq(0)
   end
   it "Check RandNum, FromOne Works" do
-    (RandNum.new.fromOne(1)).size.should eq(1)
-    (RandNum.new.fromOne(2)).size.should eq(2)
-    (RandNum.new.fromOne(3)).size.should eq(3)
-    (RandNum.new.fromOne(4)).size.should eq(4)
-    (RandNum.new.fromOne(5)).size.should eq(5)
-    (RandNum.new.fromOne(6)).size.should eq(6)
-    (RandNum.new.fromOne(7)).size.should eq(7)
-    (RandNum.new.fromOne(8)).size.should eq(8)
-    (RandNum.new.fromOne(9)).size.should eq(9)
-    (RandNum.new.fromOne(0)).size.should eq(0)
+    (RandNum.fromOne(1)).size.should eq(1)
+    (RandNum.fromOne(2)).size.should eq(2)
+    (RandNum.fromOne(3)).size.should eq(3)
+    (RandNum.fromOne(4)).size.should eq(4)
+    (RandNum.fromOne(5)).size.should eq(5)
+    (RandNum.fromOne(6)).size.should eq(6)
+    (RandNum.fromOne(7)).size.should eq(7)
+    (RandNum.fromOne(8)).size.should eq(8)
+    (RandNum.fromOne(9)).size.should eq(9)
+    (RandNum.fromOne(0)).size.should eq(0)
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,3 @@
 require "spec"
-require "../src/randomISBN"
+require "../src/RandomISBN"
+require "../src/RandomISBN/**"
